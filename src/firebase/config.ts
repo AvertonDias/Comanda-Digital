9a7366961eca3de7bc2900bd19964e75be278c62
Averton Silva Dias <verton3@gmail.com>
2026-03-02 00:18:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2526496551-97342",
+  "appId": "1:1015502597171:web:23b497253119934aae8746",
+  "apiKey": "AIzaSyCOBjfM-Uetq2lDF266gu4-2FIn7KHq_LU",
+  "authDomain": "studio-2526496551-97342.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1015502597171"
+};
